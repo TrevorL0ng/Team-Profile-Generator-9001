1e3d74468391276970916db8e9d2f05a9cc79e9c
Trevor L <trevorlong992@gmail.com>
2022-03-05 14:02:01 -0500

    Manager completed. On to the next thing

---
+++ js/Manager.js
@@ -0,0 +1,18 @@
+//Bringing in the Employee module
+const Employee = require("./Employee");
+
+//Creation and Exporting of the Manager module
+class Manager extends Employee {
+    constructor(name, email, id, officeNumber){
+        super(name, email, id);
+        this.officeNumber = officeNumber;
+        this.role = "Manager";
+    };
+    
+//Returning the only new value not extended by Employee
+    getOfficeNumber(){
+        return this.officeNumber;
+    }    
+}
+
+module.exports = "Manager"

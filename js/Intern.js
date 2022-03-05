d10bbdff4e6664aabede91d788e04894ae54b791
Trevor L <trevorlong992@gmail.com>
2022-03-05 13:56:41 -0500

    Intern completed. Easy so far. Keep the momentum going

---
+++ js/Intern.js
@@ -0,0 +1,17 @@
+//Bringing in the Employee module
+const Employee = require("./Employee");
+
+//Creation and exporting of the Intern module
+class Intern extends Employee {
+    constructor(name, email, id, school){
+        super(name, email, id);
+        this.school = school;
+        this.role = "Intern";
+    }
+//Returning new value created above not extended by Employee
+    getSchool(){
+        return this.school;
+    }    
+}
+
+module.exports = Intern;

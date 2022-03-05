28db40486243cacedc12731829dd8ff1139069b0
Trevor L <trevorlong992@gmail.com>
2022-03-05 13:43:24 -0500

    Might as well start with what you know best. Started creation of the js files. Easy enough to make an employee template

---
+++ js/Employee.js
@@ -0,0 +1,25 @@
+// Creation of and exporting of the Employee module
+class Employee {
+    constructor(name, email, id){
+        this.name = name;
+        this.email = email;
+        this.id = id;
+        this.role;
+    }
+
+//Returning all values created above 
+    getName(){
+        return this.name;
+    }
+    getEmail(){
+        return this.email;
+    }
+    getID(){
+        return this.id;
+    }
+    getRole(){
+        return this.role;
+    }
+}
+
+module.exports = Employee

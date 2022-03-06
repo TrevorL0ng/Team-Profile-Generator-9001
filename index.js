69be5355dcc1cc5fa9531018d28d2b11336de2ee
Trevor L <trevorlong992@gmail.com>
2022-03-06 17:32:40 -0500

    Added the first bit of the function. Losing drive, need food

--- index.js
+++ index.js
@@ -15,6 +15,33 @@ const output = path.join(outputDir, "main.html");
 //Questions about team to create final list
 const teammates = [];
 
+//Starting application with questions to add members to team
 function start(){
     addTeammate();
 }
+
+//Function called when teammates are to be added
+function addTeammate(){
+    inquirer.prompt([{
+        type:"list",
+        name:"role",
+        message:"Add an Engineer, Intern, or Manager to the team",
+        choices:["Engineer", "Intern", "Manager", "None"]
+    }]).then (val => {
+        if (val.role === "Engineer"){
+            engineerInfo();
+        }
+        else if(val.role === "Intern"){
+            internInfo();
+        }
+        else if(val.role === "Manager"){
+            managerInfo();
+        }
+        else{
+            writeList();
+        }
+    })
+}
+
+//Function to add an Engineer
+

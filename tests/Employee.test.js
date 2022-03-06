6c778eed3cd22d36544bdd0acb7a413c3be2c336
Trevor L <trevorlong992@gmail.com>
2022-03-06 16:28:00 -0500

    Able to copy and paste functions tested above. Now to get the role testing and we are mostly done

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,7 +1,7 @@
 //Bringing in the Employee module to create test environment
 const Employee = require("../js/Employee");
 
-//Creating test environment
+//Creating basic test environment
 test("Able to create new employee", () => {
     const i = new Employee();
     expect(typeof(i)).toBe("object");
@@ -23,4 +23,23 @@ test("Able to get ID", () => {
     const id = 3;
     const i = new Employee("Theolin", "Theolin@Strider.moc", id);
     expect(i.id).toBe(id);
+});
+
+// Setting the function test environment
+test("Able to get name with function", () => {
+    const name = "Theolin";
+    const i = new Employee(name);
+    expect(i.getName()).toBe(name);
+});
+
+test("Able to get email with function", () => {
+    const email = "Theolin@Strider.moc";
+    const i = new Employee("Theolin", email);
+    expect(i.getEmail()).toBe(email);
+});
+
+test("Able to get ID", () => {
+    const id = 3;
+    const i = new Employee("Theolin", "Theolin@Strider.moc", id);
+    expect(i.getID()).toBe(id);
 });
